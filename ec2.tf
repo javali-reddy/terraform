cda7068f2e1e3bb10cbbbeec8cea664f3b8c43d9
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-20 23:14:47 +0530

    creating public web servers

---
+++ ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "public_web_servers" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name = "infra_key"
+  subnet_id = aws_subnet.public_web_subnet[count.index].id
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  associate_public_ip_address = true
+  count=2
+
+  provisioner "file" {
+    source = "./infra_key.pem"
+    destination = "/home/ec2-user/infra_key.pem"
+  
+    connection {
+      type = "ssh"
+      host = self.public_ip
+      user = "ec2-user"
+      private_key = "${file("./infra_key.pem")}"
+    }  
+  }
+  
+
+  tags = {
+    Name = "public-web-${count.index}"
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "web-sg" {
+    name        = "web-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+  
+}
