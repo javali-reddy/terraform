817a606f6123a88c5b139795affe56c7431d3480
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-20 22:35:19 +0530

    creating igw and ngw

---
+++ ngw.tf
@@ -0,0 +1,16 @@
+resource "aws_eip" "custom_eip" {
+  domain = "vpc"
+  tags = {
+    Name="custom eip"
+  }
+}
+
+resource "aws_nat_gateway" "custom_ngw" {
+   subnet_id = aws_subnet.public_web_subnet.id
+   allocation_id = aws_eip.custom_eip.id
+tags = {
+    Name="custom ngw"
+  }
+
+  depends_on = [ aws_internet_gateway.custom-igw ]
+}
