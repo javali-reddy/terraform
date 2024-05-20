b22f62ed61aef94317a6c94a8c223ce67c61e20a
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-20 22:20:44 +0530

    creating private subnets

--- subnet.tf
+++ subnet.tf
@@ -8,3 +8,25 @@ resource "aws_subnet" "public_web_subnet" {
     Name = "public-web-subnet-${count.index}"
   }
 }
+
+resource "aws_subnet" "private_app_subnet" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = var.private_cidr[count.index]
+  availability_zone = var.az[count.index]
+  count=2
+
+  tags = {
+    Name = "private-app-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_db_subnet" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = var.var.db_cidr[count.index]
+  availability_zone = var.az[count.index]
+  count=2
+
+  tags = {
+    Name = "private-db-subnet-${count.index}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "public_cidr" {
 variable "az" {
   default = ["us-east-1a","us-east-1b"]
 }
+
+variable "private_cidr" {
+  default = ["172.20.3.0/24","172.20.4.0/24"]
+}
+
+variable "db_cidr" {
+  default = ["172.20.5.0/24","172.20.6.0/24"]
+}
