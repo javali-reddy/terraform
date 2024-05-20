2596ab76b7e8ba1edc5e0f0ac015e22722b4d7b5
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-20 22:50:25 +0530

    creating route tables

---
+++ route.tf
@@ -0,0 +1,42 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom-igw.id
+  }
+
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.custom_ngw.id
+  }
+
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_web_subnet.*.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_app_assoc" {
+  subnet_id      = aws_subnet.private_app_subnet.*.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_db_assoc" {
+  subnet_id      = aws_subnet.private_db_subnet.*.id
+  route_table_id = aws_route_table.private_rt.id
+}
