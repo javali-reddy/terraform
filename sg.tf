5e64dedc572b4cdd66bf9fd6afc55f477eb775b8
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-21 21:03:19 +0530

    adding changes to code

--- sg.tf
+++ sg.tf
@@ -24,28 +24,28 @@ resource "aws_security_group" "web-sg" {
   
 }
 
-resource "aws_security_group" "app-sg" {
-  name        = "app-sg"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.custom_vpc.id
+# resource "aws_security_group" "app-sg" {
+#   name        = "app-sg"
+#   description = "Allow inbound traffic"
+#   vpc_id      = aws_vpc.custom_vpc.id
 
-  ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_security_group.web-sg.id]
-  }
+#   ingress {
+#     description      = "SSH"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = [aws_security_group.web-sg.id]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "app-sg"
-  }
+#   tags = {
+#     Name = "app-sg"
+#   }
   
-}
+# }
