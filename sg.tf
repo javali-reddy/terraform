83b04614c9db7f406ea7f10258ec1c9adcf741f9
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-21 20:00:59 +0530

    creating private app servers

--- sg.tf
+++ sg.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "app-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [aws_security_group.web-sg]
   }
 
   egress {
