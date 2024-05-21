d1e92380ea4476b0af37f4d64c059164a638d90b
Javali Valluru <javalivallururjsj44@gmail.com>
2024-05-21 19:56:17 +0530

    creating private app servers

--- sg.tf
+++ sg.tf
@@ -23,3 +23,29 @@ resource "aws_security_group" "web-sg" {
   }
   
 }
+
+resource "aws_security_group" "app-sg" {
+  name        = "app-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-sg"
+  }
+  
+}
